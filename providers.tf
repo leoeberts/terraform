e8b387072caf8b32aeff550e3d74b680b2175572
Leonardo Eberts <leonardo.eberts@gmail.com>
2025-06-16 18:28:59 +0200

    Provider version constraint

--- providers.tf
+++ providers.tf
@@ -1,15 +1,19 @@
-provider "aws" {
-  region  = "eu-central-1"
-  profile = "terraform-study" 
-}
-
 terraform {
     required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 5.0"
+        }
         github = {
             source = "integrations/github"
             version = "~> 6.0"
         }
     }
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+  profile = "terraform-study" 
 }
 
 provider "github" {
