317b9aafc296bf112b2f401b681240fb0606ef4e
Leonardo Eberts <leonardo.eberts@gmail.com>
2025-07-07 18:22:00 +0200

    Provisioner - Remote

--- 3.provisioners/data.tf
+++ 3.provisioners/data.tf
@@ -24,3 +24,17 @@ output "linux_ami" {
     name = data.aws_ami.latest_linux.name
   }
 }
+
+data "aws_key_pair" "study_key" {
+  filter {
+    name   = "key-pair-id"
+    values = ["key-0ce3c988a890c59fc"]
+  }
+}
+
+output "study_key" {
+  value = {
+    id   = data.aws_key_pair.study_key.id
+    name = data.aws_key_pair.study_key.key_name
+  }
+}
--- 3.provisioners/main.tf
+++ 3.provisioners/main.tf
@@ -5,6 +5,7 @@ locals {
   }
   port = {
     ssh = "22",
+    web = "80",
   }
   everywhere_cidr_v4 = "0.0.0.0/0"
   vpc_id             = "vpc-06c09fea3a4914da8"
@@ -30,16 +31,39 @@ resource "aws_security_group_rule" "allow_ssh_ipv4" {
   cidr_blocks       = [local.everywhere_cidr_v4]
 }
 
+resource "aws_security_group_rule" "allow_web_ipv4" {
+  security_group_id = aws_security_group.web_firewall.id
+  type              = "ingress"
+  protocol          = local.protocol.tcp
+  from_port         = local.port.web
+  to_port           = local.port.web
+  cidr_blocks       = [local.everywhere_cidr_v4]
+}
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.latest_linux.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.web_firewall.id]
+  key_name               = data.aws_key_pair.study_key.key_name
   tags = {
-    Name = "terraform-test-web"
+    Name = "terraform-test-web-2"
   }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.web.public_ip} > public_ip.txt"
+    command = "echo ${self.public_ip} > public_ip.txt"
+  }
+
+  connection {
+    host        = self.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("Study.pem")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx",
+    ]
   }
 }
