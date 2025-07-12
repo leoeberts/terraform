334edd8432954a727b6b312ba4c401e1404306a7
Leonardo Eberts <leonardo.eberts@gmail.com>
2025-07-12 21:46:37 +0200

    Modules - use module that requires info

--- 4.modules_workspaces/4.1.using_module_example/ec2_instance.tf
+++ 4.modules_workspaces/4.1.using_module_example/ec2_instance.tf
@@ -1,4 +1,4 @@
-module "ec2-instance" {
+module "ec2-instance" { #some modules do not require input variables
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.1"
 }
---
+++ 4.modules_workspaces/4.1.using_module_example/eks.tf
@@ -0,0 +1,6 @@
+/*module "eks" { #some modules require input variables
+  source       = "terraform-aws-modules/eks/aws"
+  version      = "20.37.1"
+  cluster_name = "my-eks-cluster"
+  subnet_ids   = ["subnet-04c25b7f48e97d567"]
+}*/ #commneted so it is not created by mistake
